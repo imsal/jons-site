60a97c0b80d68aef4e5d71f2ff382668a4e852e8
Sal Tardibuono <s.tardibuono@live.com>
2019-02-13 12:54:44 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// change to on
+
+
+// var fullHeightFullWidth = function() {
+//
+// }
+
+
+$( document ).ready(function() {
+  $("#full-height-full-width").css('height', '100vh');
+  $("#full-height-full-width").css('width', '100vw');
+});
